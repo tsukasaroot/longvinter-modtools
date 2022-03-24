a5726017c940b62ace8eefcdfdf33574b88c6e91
Jordan <jordan.rouco-miguez@epitech.eu>
2022-03-24 12:30:48 +0100

    deleted consolelog + adding a builder.sh

--- public/lib/source/update.js
+++ public/lib/source/update.js
@@ -4,7 +4,6 @@ function update(mod_name, t) {
 
     ipcRenderer.send('update', storage.getItem(mod_name));
     ipcRenderer.on('update', (event, args) => {
-        console.log(t.parentNode.parentNode.children[1])
         t.parentNode.parentNode.children[1].innerHTML = JSON.parse(storage.getItem(mod_name)).version;
         t.parentNode.removeChild(t);
     });
